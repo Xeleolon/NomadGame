c4685bd1f50c7b71e266c128497e1819df957bff
Xeleolon <nathan.cattell@outlook.com>
2024-03-03 19:36:14 +0800

    NomadV0.02

--- Nomad/Assets/Scripts/Player/PlayerLife.cs
+++ Nomad/Assets/Scripts/Player/PlayerLife.cs
@@ -6,8 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerLife : MonoBehaviour
 {
     [Header("Health")]
+    [SerializeField] bool freazeDecays;
     [SerializeField] float health;
     float curHealth;
+    [SerializeField] float thirst;
+    float curThrist;
+    [SerializeField] float thirstDecay;
+    [SerializeField] float hunger;
+    float curHunger;
+    [SerializeField] float hungerDecay;
+    
+    [Header("Paused")]
+    public bool gamePaused;
     [Header("Actack")]
     float coolDown;
     public WeaponItem weapon;
@@ -35,6 +45,8 @@ public class PlayerLife : MonoBehaviour
     void Start()
     {
         curHealth = health;
+        curHunger = hunger;
+        curThrist = thirst;
     }
 
     void Update()
@@ -43,12 +55,59 @@ public class PlayerLife : MonoBehaviour
         {
             coolDown -= 1 * Time.deltaTime;
         }
+
+        if (!gamePaused)
+        {
+            if (!freazeDecays)
+            {
+                SurvivalDecays();
+            }
+        }
+
     }
+    #region Survial
+
+    void SurvivalDecays()
+    {
+        if (curThrist > 0)
+        {
+            curThrist -= thirstDecay * Time.deltaTime;
+        }
+        else
+        {
+            AlterHealth(0.2 * Time.deltaTime);
+        }
+
+        if (curHunger > 0)
+        {
+            curHunger -= thirstHunger * Time.deltaTime;
+        }
+        else
+        {
+            AlterHealth(0.2 * Time.deltaTime);
+        }
+    }
+
+    public void AlterHealth(float temp)
+    {
+        curHealth += temp;
+
+        if (curHealth <= 0)
+        {
+            Debug.Log("Player Died");
+        }
+        else if (curHealth > health)
+        {
+            curHealth = health;
+        }
+
+    }
+    #endregion
     #region Actacks
 
     void Actack(InputAction.CallbackContext context)
     {
-        if (coolDown <= 0 )
+        if (coolDown <= 0 && !gamePaused)
         {
             switch (weapon.type)
             {
