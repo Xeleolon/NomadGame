f77ab64559c30ceb0577b1e403cdcf762482ac7d
Xeleolon <nathan.cattell@outlook.com>
2024-08-12 23:11:45 +0800

    nomadV1.46

--- Nomad/Assets/Scripts/Emeny/Boss/BossStages.cs
+++ Nomad/Assets/Scripts/Emeny/Boss/BossStages.cs
@@ -4,15 +4,94 @@ using UnityEngine;
 
 public class BossStages : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject projectilePrefab;
+
+    private int stage = 1;
+
+    private enum StageProgress { inactive, actack1, actack2, actack3, exposed }
+    StageProgress stageProgress = StageProgress.inactive;
+
     void Start()
     {
-        
+        stageProgress = StageProgress.actack1;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        switch (stage)
+        {
+            case 1:
+                StageOne();
+            break;
+
+            case 2:
+                StageTwo();
+            break;
+
+            case 3:
+                StageThree();
+            break;
+        }
+    }
+
+    void StageOne()
+    {
+        switch (stageProgress)
+        {
+            case StageProgress.actack1:
+                //play animation for projectile
+                stageProgress = StageProgress.inactive;
+
+                break;
+
+            case StageProgress.exposed:
+                //player animation for exposing weakness
+                stageProgress = StageProgress.inactive;
+
+                break;
+        }
+    }
+
+    void StageTwo()
+    {
+        switch (stageProgress)
+        {
+            case StageProgress.actack1:
+                //play animation for projectile
+                stageProgress = StageProgress.inactive;
+
+                break;
+
+            case StageProgress.exposed:
+                //player animation for exposing weakness
+                stageProgress = StageProgress.inactive;
+
+                break;
+        }
+    }
+
+    void StageThree()
+    {
+        switch (stageProgress)
+        {
+            case StageProgress.actack1:
+                //play animation for projectile
+                stageProgress = StageProgress.inactive;
+
+                break;
+
+            case StageProgress.exposed:
+                //player animation for exposing weakness
+                stageProgress = StageProgress.inactive;
+
+                break;
+        }
+    }
+
+    public void FireProjectile()
+    {
         
+
     }
 }
